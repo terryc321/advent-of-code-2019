b74863dea39500ed68a4224859e7918900c7dda1
terry <terry.cadd@gmail.com>
2026-02-22 20:31:19 +0000

    are we bailing again ?
    getting bit complicated on parameter modes

--- day7/chicken/day2.scm
+++ day7/chicken/day2.scm
@@ -1,9 +1,140 @@
 
 ;; chicken scheme
+(import (chicken format))
 
 
 (define (input)
-  (list 3 8 1001 8 10 8 105 1 0 0 21 46 67 76 97 118 199 280 361 442 99999 3 9 1002 9 3 9 101 4 9 9 102 3 9 9 1001 9 3 9 1002 9 2 9 4 9 99 3 9 102 2 9 9 101 5 9 9 1002 9 2 9 101 2 9 9 4 9 99 3 9 101 4 9 9 4 9 99 3 9 1001 9 4 9 102 2 9 9 1001 9 4 9 1002 9 5 9 4 9 99 3 9 102 3 9 9 1001 9 2 9 1002 9 3 9 1001 9 3 9 4 9 99 3 9 101 1 9 9 4 9 3 9 1001 9 2 9 4 9 3 9 1001 9 1 9 4 9 3 9 1001 9 1 9 4 9 3 9 101 2 9 9 4 9 3 9 102 2 9 9 4 9 3 9 1002 9 2 9 4 9 3 9 1002 9 2 9 4 9 3 9 1001 9 2 9 4 9 3 9 101 1 9 9 4 9 99 3 9 102 2 9 9 4 9 3 9 101 2 9 9 4 9 3 9 102 2 9 9 4 9 3 9 1002 9 2 9 4 9 3 9 102 2 9 9 4 9 3 9 1001 9 2 9 4 9 3 9 1001 9 1 9 4 9 3 9 102 2 9 9 4 9 3 9 101 1 9 9 4 9 3 9 101 2 9 9 4 9 99 3 9 1002 9 2 9 4 9 3 9 1001 9 1 9 4 9 3 9 101 2 9 9 4 9 3 9 1002 9 2 9 4 9 3 9 102 2 9 9 4 9 3 9 1001 9 1 9 4 9 3 9 1002 9 2 9 4 9 3 9 101 1 9 9 4 9 3 9 1002 9 2 9 4 9 3 9 1001 9 1 9 4 9 99 3 9 1001 9 2 9 4 9 3 9 1002 9 2 9 4 9 3 9 1002 9 2 9 4 9 3 9 101 2 9 9 4 9 3 9 1001 9 1 9 4 9 3 9 101 1 9 9 4 9 3 9 1001 9 2 9 4 9 3 9 1001 9 1 9 4 9 3 9 1002 9 2 9 4 9 3 9 1001 9 1 9 4 9 99 3 9 1002 9 2 9 4 9 3 9 101 2 9 9 4 9 3 9 1002 9 2 9 4 9 3 9 101 2 9 9 4 9 3 9 1001 9 2 9 4 9 3 9 101 1 9 9 4 9 3 9 102 2 9 9 4 9 3 9 102 2 9 9 4 9 3 9 1001 9 2 9 4 9 3 9 1002 9 2 9 4 9 99))
+  (list->vector (list 1 0 0 3 1 1 2 3 1 3 4 3 1 5 0 3 2 13 1 19 1 19 10 23 2 10 23 27 1 27 6 31 1 13 31 35 1 13 35 39 1 39 10 43 2 43 13 47 1 47 9 51 2 51 13 55 1 5 55 59 2 59 9 63 1 13 63 67 2 13 67 71 1 71 5 75 2 75 13 79 1 79 6 83 1 83 5 87 2 87 6 91 1 5 91 95 1 95 13 99 2 99 6 103 1 5 103 107 1 107 9 111 2 6 111 115 1 5 115 119 1 119 2 123 1 6 123 0 99 2 14 0 0)))
 
+;; (define (mk-state v i)
+;;   (vector 'state v i))
+
+;; (define (state? s)
+;;   (and (vector? s)(eq? (vector-ref s 0) 'state)))
+
+;; (define (state-vector s)
+;;   (cond
+;;    ((state? s) (vector-ref s 1))
+;;    (#t (error "not a state"))))
+
+;; (define (state-index s)
+;;   (cond
+;;    ((state? s) (vector-ref s 2))
+;;    (#t (error "not a state"))))
+
+;; (define (state-read s i)
+;;   (cond
+;;    ((state? s) (vector-ref (state-vector s) i))
+;;    (#t (error "not a state"))))
+
+;; (define (state-write s i n)
+;;   (cond
+;;    ((state? s) (let* ((v1 (state-vector s))
+;; 		      (vlen (vector-length v1))
+;; 		      (v2 (make-vector vlen)))
+;; 		 (vector-copy! v1 v2)
+;; 		 (vector-set! v2 i n)
+;; 		 (mk-state v2 i)))
+;;    (#t (error "not a state"))))
+
+;; (define (initial-state)
+;;   (mk-state (input) 0))
+
+(define verbose #f)
+
+;; copy by coercion
+(define (vector-copy v)
+  (list->vector (vector->list v)))
+
+
+
+(define (sim v i)
+  (let ((op (vector-ref v i)))
+    (cond
+     ((= op 1) (sim-add v i))
+     ((= op 2) (sim-mul v i))
+     ((= op 3) (sim-store v i))     
+     ((= op 99)
+      (when verbose (format #t "sim-halted ~a ~%" v))
+      v)
+     (#t (error "bad state")))))
+
+
+(define (sim-add v i)
+  (let* ((i1 (vector-ref v (+ i 1)))
+	 (i2 (vector-ref v (+ i 2)))
+	 (n1 (vector-ref v i1))
+	 (n2 (vector-ref v i2))
+	 (dest (vector-ref v (+ i 3)))	 
+	 (r1 (+ n1 n2))
+	 (v2 (vector-copy v)))    
+    (when verbose
+      (format #t "sim-add ~a ~a at ~a => ~a ~%" n1 n2 dest r1))
+    (vector-set! v2 dest r1)
+    (sim v2 (+ i 4))))
+
+
+(define (sim-mul v i)
+  (let* ((i1 (vector-ref v (+ i 1)))
+	 (i2 (vector-ref v (+ i 2)))
+	 (n1 (vector-ref v i1))
+	 (n2 (vector-ref v i2))
+	 (dest (vector-ref v (+ i 3)))	 
+	 (r1 (* n1 n2))
+	 (v2 (vector-copy v)))    
+    (when verbose
+      (format #t "sim-mul ~a ~a at ~a => ~a ~%" n1 n2 dest r1))
+    (vector-set! v2 dest r1)
+    (sim v2 (+ i 4))))
+
+
+;; using read -- but where does read get its input from ?
+(define (sim-store v i)
+  (let* ((dest (vector-ref v (+ i 1)))	 
+	 (v2 (vector-copy v)))    
+    (when verbose
+      (format #t "sim-store ~a => ~a ~%" dest))
+    (vector-set! v2 dest (read))
+    (sim v2 (+ i 2))))
+
+
+;; 1,0,0,0,99 becomes 2,0,0,0,99 (1 + 1 = 2).
+;; 2,3,0,3,99 becomes 2,3,0,6,99 (3 * 2 = 6).
+;; 2,4,4,5,99,0 becomes 2,4,4,5,99,9801 (99 * 99 = 9801).
+;; 1,1,1,4,99,5,6,0,99 becomes 30,1,1,4,2,5,6,0,99.
+
+(define (example-trial xs)  (sim (list->vector xs) 0))
+(define (ex1)  (example-trial '(1 0 0 0 99)))
+(define (ex2)  (example-trial '(2 3 0 3 99)))
+(define (ex3)  (example-trial '(2 4 4 5 99 0)))
+(define (ex4)  (example-trial '(1 1 1 4 99 5 6 0 99)))
+
+;; replace 1 with 12
+;; replace 2 with 2
+(define (day2-part1)
+  (let ((in (input)))
+    (vector-set! in 1 12)
+    (vector-set! in 2 2)
+    (sim in 0)))
+
+
+(define (day2-part2)
+  (let ((target 19690720))
+    (letrec ((rec (lambda (n v)
+		    (cond
+		     ((> v 99) #f)
+		     ((> n 99) (rec 0 (+ v 1)))
+		     (#t (let ((in (input)))
+			   (vector-set! in 1 n)
+			   (vector-set! in 2 v)
+			   (let ((out (sim in 0)))
+			     (cond
+			      ((= (vector-ref out 0) target)
+			       (let ((nv100 (+ (* 100 n) v)))
+				 (format #t "n ~a : v ~a : 100n+v = ~a ~%" n v nv100)))
+			      (#t (rec (+ n 1) v))))))))))
+      (rec 0 0))))
+
+;; gets a little bit involved now...
 
 
--- day7/haskell/app/Main.hs
+++ day7/haskell/app/Main.hs
@@ -1,4 +1,85 @@
 module Main where
 
+import Data.Sequence as S
+
+{-
+Your wish is my IO ().
+If I break, you can:
+  1. Restart:           M-x haskell-process-restart
+  2. Configure logging: C-h v haskell-process-log (useful for debugging)
+  3. General config:    M-x customize-mode
+  4. Hide these tips:   C-h v haskell-process-show-debug-tips
+-}
+
+--- C-c C-l to load into ghci
+-- input :: S.Seq Integer
+--- changed element 0 to 123 was 1
+--- changed last element to 321 was 0
+input :: Seq Integer 
+input = S.fromList [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,13,1,19,1,19,10,23,2,10,23,27,1,27,6,31,1,13,31,35,1,13,35,39,1,39,10,43,2,43,13,47,1,47,9,51,2,51,13,55,1,5,55,59,2,59,9,63,1,13,63,67,2,13,67,71,1,71,5,75,2,75,13,79,1,79,6,83,1,83,5,87,2,87,6,91,1,5,91,95,1,95,13,99,2,99,6,103,1,5,103,107,1,107,9,111,2,6,111,115,1,5,115,119,1,119,2,123,1,6,123,0,99,2,14,0,0]
+--DS.length (DS.fromList input) => 133
+--S.length (S.fromList input) => 133
+--S.lookup 0 -- Just 1 -- back to monads now 
+--S.lookup 132 -- Just 0 -- back to monads now
+
+--- index
+
+--- 1 adds
+--- 2 multiplies
+--- 99 halts
+
+--- tape === Seq Integer
+
+-- data Tape = Tape_Seq (Seq Integer)
+-- data Index = Index_Int Int 
+data State = Sok (Seq Integer) Integer
+             | Shalt (Seq Integer) Integer
+             | Sbad (Seq Integer) Integer
+
+
+             
+
+--- some kind of state ?
+--- t :: tape
+--- i index into tape
+--- ?? history of moves
+sim :: State -> State 
+sim (Sbad s i ) = Sbad s i
+sim (Shalt s i ) = Shalt s i
+sim (Sok s i ) = let len = S.length s
+                 in if (i >= 0 && i < len) then
+                      case S.lookup i s of 
+                      Nothing -> Sbad s i
+                      Just n -> case n of
+                        1 -> sim_add s i 
+                        2 -> sim_mul s i
+                        99 -> Shalt s i
+                        _ -> Sbad s i
+                    else Sbad s i
+
+
+sim_add s i = let len = S.length s
+              in case (S.lookup (i+1) s, S.lookup (i+2) s, S.lookup (i+3) s) of
+                (Just a,Just b, Just c) -> case c of
+                                             if (c >= 0 && c < len) then
+                                               let s2 = S.update c (a+b) s
+                                               in sim (Sok s2 (i + 4))
+                                             else Sbad s i
+                _ -> Sbad s i 
+                                                 
+
+sim_mul s i = let len = S.length s
+              in case (S.lookup (i+1) s, S.lookup (i+2) s, S.lookup (i+3) s) of
+                (Just a,Just b, Just c) -> case c of
+                                             if (c >= 0 && c < len) then
+                                               let s2 = S.update c (a*b) s
+                                               in sim (Sok s2 (i + 4))
+                                             else Sbad s i
+                _ -> Sbad s i 
+                                                 
+
+
+
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
